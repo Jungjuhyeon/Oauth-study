abf3d35b5abfe22957e7980a9579ec95002ba88f
jungjoo <wjddis000507@gmail.com>
2024-07-11 13:37:31 +0900

    07/11 study start

---
+++ src/main/java/Oauth_study/Oauth/OauthStudyApplication.java
@@ -0,0 +1,13 @@
+package Oauth_study.Oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Oauth_study/demo/OauthStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package Oauth_study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
